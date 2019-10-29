a9428b4b151f65830f59441a62a3abe031234eef
danbrannon <dbrannon@cityofripon.org>
2019-10-29 16:23:14 -0700

    Embedded Google Webfonts

--- City-Hall-Conference/index.php
+++ City-Hall-Conference/index.php
@@ -4,13 +4,14 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <meta http-equiv="refresh" content="60">
 <title>A/V Control</title>
+<link href="https://fonts.googleapis.com/css?family=Patua+One|Roboto|Roboto+Condensed|Roboto+Slab&display=swap" rel="stylesheet">
 </head>
 <body style="background-image:url(tree.jpg);background-position:center;background-repeat:no-repeat;background-size:cover;">
 <form method="post">
-		<div style="text-align: center; font-family: Arial;font-size:36pt;color:#fff;font-weight:600;margin-bottom:25px">
+		<div style="text-align: center; font-family: 'Patua One', cursive;font-size:36pt;color:#fff;font-weight:600;margin-bottom:25px">
 			City Hall Conference Room AV Control
 			</div>
-<table style="width:98%;background-image:url(logo.png);background-repeat:no-repeat;background-position:center;z-index:5;margin-left:1%;margin-right:1%">
+<table style="width:98%;background-image:url(logo.png);background-repeat:no-repeat;background-position:center;z-index:5;margin-left:1%;margin-right:1%font-family: 'Roboto', sans-serif;">
 <tr>
 		<td>
 			<button name="vol-up"class="btn btn-primary" style="width:235; height: 125; font-size: 18pt; font-weight:400;" >Volume Down</button>
@@ -27,6 +28,7 @@
 		<br>
 		<button name="sys-off"class="btn btn-danger" style="width:235; height:200; font-size: 22pt;" >SYSTEM OFF</button>
 		</td>
+		
 </tr>
 <tr style="height:80px;>"
 <td></td>
