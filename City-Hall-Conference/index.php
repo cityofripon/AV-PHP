a704a746ebe16c1831ddcd5e66eee07f98e39bb8
danbrannon <dbrannon@cityofripon.org>
2019-10-29 16:44:54 -0700

    MOTS!

--- City-Hall-Conference/index.php
+++ City-Hall-Conference/index.php
@@ -39,7 +39,7 @@
 	<td>
 	</td>
 	<td style="text-align:center;">
-		<button name="table-vga" style="width:235; height: 103; font-size: 18pt; font-weight:400;padding-left:0pt;padding-right:10pt;" ><img src="table-vga.png"></button>
+		<button name="table-vga" style="width:235; height: 103; font-size: 18pt; font-weight:400;padding-left:0pt;padding-right:10pt;" ><img src="table-vga.png" width="235" height="103"></button>
 		<button name="table-hdmi" style="width:235; height: 103; font-size: 18pt; font-weight: 400;padding-left:10pt;padding-right:10pt;" ><img src="table-hdmi.png"></button>
 		<button name="clickshare"class="btn btn-secondary" style="width:235; height: 103; font-size: 18pt; font-weight: 400;padding-left:10pt;padding-right:0pt;" >HDMI<br>INPUT</button>
 	</td>
