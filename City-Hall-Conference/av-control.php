3b2e1333981c9ccaf70eaadefc07b5ca9afcb381
danbrannon <dbrannon@cityofripon.org>
2019-11-22 15:15:38 -0800

    asdf

--- City-Hall-Conference/av-control.php
+++ City-Hall-Conference/av-control.php
@@ -58,247 +58,127 @@ if(isset($_POST['sys-off']))
 	system("gpio write 28 0");
 	sleep(1);
 	system("gpio write 28 1");
-
 //power off projector
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB0");
-	$serial->confBaudRate(9600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
+	$pjserial->deviceOpen();
 	$cmd = "PWR OFF\r";
-
+	$pjserial->sendMessage($cmd);
+	$pjserial->deviceClose();
 // Mute Sound
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
+	$swserial->deviceOpen();
 	$cmd = "SET VOLGAIN_DATA audioout 0\r";
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
 
 // Table VGA Button
 if(isset($POST['source-table-vga']))
 {
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
+	$swserial->deviceOpen();
 	$cmd = "SET SW vga\r";
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
 // Table HDMI Button
 if(isset($POST['source-table-hdmi']))
 {
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
+	$swserial->deviceOpen();
 	$cmd = "SET SW HDMI2\r\n";
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
 // Clickshare Button
 if(isset($POST['source-clickshare']))
 {
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
+	$swserial->deviceOpen();
 	//$cmd = "SET SW dp\r";
 	$cmd = "SET SW HDMI1\r\n";
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
 // DVD/Blu-Ray Button
 if(isset($POST['source-dvd-bluray']))
 {
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
-	$cmd = "SET SW HDMI1\r";
-	$serial->sendMessage($cmd);
-	$serial->deviceClose();
+$swserial->deviceOpen();
+$cmd = "SET SW HDMI1\r";
+$swserial->sendMessage($cmd);
+$swserial->deviceClose();
 }
-if(isset($POST['vol-dn']))
-{
-	$serial = new PhpSerial;
-	$serial->deviceSet("/dev/ttyUSB1");
-	$serial->confBaudRate(57600);
-	$serial->confParity("none");
-	$serial->confCharacterLength(8);
-	$serial->confStopBits(1);
-	$serial->deviceOpen();
-	$cmd = "SET MUTE audioout off\r";
-	$serial->sendMessage($cmd);
-	$read = $serial->readPort();
-	// Print out the data
-	echo $read;
-	$serial->deviceClose();
-	}
+//Volume Settings
 if(isset($POST['vol-100']))
 {
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 100\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 100\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
-	if(isset($POST['vol-90']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 90\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-80']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 80\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-70']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 70\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-60']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 60\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-50']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 50\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-40']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 40\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-30']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 30\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-20']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB1");
-		$serial->confBaudRate(57600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "SET VOLGAIN_DATA audioout 20\r";
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
-	}
-	if(isset($POST['vol-10']))
-	{
-		$serial = new PhpSerial;
-		$serial->deviceSet("/dev/ttyUSB0");
-		$serial->confBaudRate(9600);
-		$serial->confParity("none");
-		$serial->confCharacterLength(8);
-		$serial->confStopBits(1);
-		$serial->deviceOpen();
-		$cmd = "MUTE ON\r";
-	//	$cmd = pack("H*",$cmd);
-		$serial->sendMessage($cmd);
-		$serial->deviceClose();
+if(isset($POST['vol-90']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 90\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
 }
-		if(isset($POST['vol-0']))
-		{
-			$serial = new PhpSerial;
-			$serial->deviceSet("/dev/ttyUSB1");
-			$serial->confBaudRate(57600);
-			$serial->confParity("none");
-			$serial->confCharacterLength(8);
-			$serial->confStopBits(1);
-			$serial->deviceOpen();
-			$cmd = "SET VOLGAIN_DATA audioout 0\r";
-			$serial->sendMessage($cmd);
-			$serial->deviceClose();
-		}
+if(isset($POST['vol-80']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 80\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-70']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 70\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-60']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 60\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-50']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 50\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-40']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 40\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-30']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 30\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-20']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 20\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-10']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 10\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
+if(isset($POST['vol-0']))
+{
+	$swserial->deviceOpen();
+	$cmd = "SET VOLGAIN_DATA audioout 00\r";
+	$swserial->sendMessage($cmd);
+	$swserial->deviceClose();
+}
 ?>
