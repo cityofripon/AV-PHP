0d8be8a01762a975bdc8d63aa1d582238c4ceff0
danbrannon <dbrannon@cityofripon.org>
2020-05-14 10:46:51 -0700

    asdf

--- City-Hall-Conference/index.php
+++ City-Hall-Conference/index.php
@@ -27,15 +27,15 @@ $read = "";
 		<td>
 				<button name="vol-max"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >Volume Max</button>
 <br>
-				<button name="vol-5"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >5</button>
+				<button name="vol-5"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 3pt;" >5</button>
 <br>
-				<button name="vol-4"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >4</button>
+				<button name="vol-4"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 2pt;" >4</button>
 <br>
-				<button name="vol-3"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >3</button>
+				<button name="vol-3"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 2pt;" >3</button>
 <br>
-				<button name="vol-2"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >2</button>
+				<button name="vol-2"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 2pt;" >2</button>
 <br>
-				<button name="vol-1"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 5pt;" >1</button>
+				<button name="vol-1"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400; margin-bottom: 2pt;" >1</button>
 <br>
 				<button name="vol-mute"class="btn btn-primary" style="width:235; height: 45; font-size: 24pt; font-weight:400;" >Volume Mute</button>
 		</td>
